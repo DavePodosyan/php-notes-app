c98421c496f96ba52e6220bbf07310533f1f3bba
David <d.podosyan@gmail.com>
2023-05-03 21:21:11 +0400

    First Commit

---
+++ views/partials/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100" lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+    <title><?php echo isset($heading) ? $heading : '404'; ?></title>
+</head>
+
+<body class="h-full">
+
+    <div class="min-h-full">
